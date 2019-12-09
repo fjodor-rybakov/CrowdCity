0c9b9ec6c3e2050d02c35abe931979f6538e19e4
fjodor-rybakov <fedryb@mail.ru>
2019-12-09 23:49:52 +0300

    add base project

---
+++ Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject npcList;
+    public Terrain terrain;
+    public GameObject npcPrefab;
+    public float yOffset = 0.5f;
+    public int countMinNpc = 3;
+
+    private float _terrainWidth;
+    private float _terrainLength;
+
+    private float _xTerrainPos;
+    private float _zTerrainPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get terrain size
+        var terrainData = terrain.terrainData;
+        _terrainWidth = terrainData.size.x;
+        _terrainLength = terrainData.size.z;
+
+        //Get terrain position
+        var position = terrain.transform.position;
+        _xTerrainPos = position.x;
+        _zTerrainPos = position.z;
+    }
+
+    private void GenerateObjectOnTerrain()
+    {
+        //Generate random x,z,y position on the terrain
+        var randX = Random.Range(_xTerrainPos, _xTerrainPos + _terrainWidth);
+        var randZ = Random.Range(_zTerrainPos, _zTerrainPos + _terrainLength);
+        var yVal = Terrain.activeTerrain.SampleHeight(new Vector3(randX, 0, randZ));
+
+        //Apply Offset if needed
+        yVal += yOffset;
+
+        //Generate the Prefab on the generated position
+        var objInstance = Instantiate(npcPrefab, new Vector3(randX, yVal, randZ), Quaternion.identity);
+        objInstance.transform.SetParent(npcList.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddNpcList();
+    }
+
+    private void AddNpcList()
+    {
+        if (npcList.transform.childCount < countMinNpc)
+            GenerateObjectOnTerrain();
+    }
+}
---
+++ Assets/Resources/Scripts/Move.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject npcList;
+    public int triggeredDistance = 2;
+    public GameObject groupParent;
+    
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToLocation();
+        Grouping();
+    }
+
+    private void Grouping()
+    {
+        for (var i = 0; i < npcList.transform.childCount; i++)
+        {
+            var item = npcList.transform.GetChild(i);
+            var distance = Vector3.Distance(item.position, _agent.transform.position);
+            if (!(distance <= triggeredDistance)) continue;
+            AddToGroup(ref item);
+        }
+    }
+
+    private void AddToGroup(ref Transform item)
+    {
+        item.transform.SetParent(groupParent.transform);
+        var agentComponent = item.transform.gameObject.AddComponent<NavMeshAgent>();
+        agentComponent.speed = 8;
+        agentComponent.acceleration = 10;
+        var moveSComponent = item.transform.gameObject.AddComponent<Move>();
+        moveSComponent.mainCamera = mainCamera;
+        moveSComponent.npcList = npcList;
+        moveSComponent.groupParent = groupParent;
+    }
+
+    private void MoveToLocation()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out var hit, 1000.0f)) return;
+        _agent.SetDestination(new Vector3(hit.point.x, 0, hit.point.z));
+    }
+}
